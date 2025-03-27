c1ae0c7bb5127020a0c11a36b3920d494f2a599e
Kelianne235 <stpk1601@Usherbrooke.ca>
2025-03-27 15:34:25 -0400

    On essai une autre fonction

--- Eval1.R
+++ Eval1.R
@@ -8,4 +8,26 @@ convertion_array_list <- function(x) {
   return(num_values)
 }
 
+unnest_column <- function(df, colname, sep = ",") {
+  # Vérifie si la colonne existe dans le dataframe
+  if (!colname %in% names(df)) {
+    stop("La colonne spécifiée n'existe pas dans le dataframe.")
+  }
+  
+  # Enlever les crochets et les espaces supplémentaires dans la colonne
+  df[[colname]] <- gsub("\\[|\\]", "", as.character(df[[colname]]))  # Enlève les crochets
+  df[[colname]] <- gsub("\\s+", "", df[[colname]])  # Enlève les espaces inutiles
+  
+  # Convertir la colonne en liste en fonction du séparateur spécifié
+  df[[colname]] <- strsplit(as.character(df[[colname]]), sep)
+  
+  # Appliquer unnest() pour transformer chaque élément de la liste en ligne séparée
+  df_unnested <- df %>%
+    tidyr::unnest(!!sym(colname))  # Le sym() permet de référencer la colonne dynamiquement
+  
+  # Convertir les années en nombres
+  df_unnested[[colname]] <- as.numeric(df_unnested[[colname]])  # Conversion en numérique
+  
+  return(df_unnested)
+}
 
