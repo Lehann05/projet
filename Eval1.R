5057faa86da895bc7889482ca5894160799d3a2b
Kelianne235 <stpk1601@Usherbrooke.ca>
2025-03-27 13:44:26 -0400

    Enlever la fonction de trop

--- Eval1.R
+++ Eval1.R
@@ -8,17 +8,4 @@ convertion_array_list <- function(x) {
   return(num_values)
 }
 
-# Fonction pour forcer une colonne en liste
-force_list <- function(df, colname) {
-  if (colname %in% names(df)) {
-    if (!is.list(df[[colname]])) {
-      df[[colname]] <- lapply(df[[colname]], function(x) {
-        if (is.character(x)) {
-          return(as.list(convertion_array_list(x)))
-        }
-        return(as.list(x))
-      })
-    }
-  }
-  return(df)
-}
+
