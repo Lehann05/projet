43b8a971600ae37da8126fd527798ffe8661ced4
Kelianne235 <stpk1601@Usherbrooke.ca>
2025-04-04 14:13:02 -0400

    Tests pour unnest sans duplication (kinda failing at it)

--- Eval1.R
+++ Eval1.R
@@ -27,3 +27,46 @@ unnest_column <- function(df, colname, sep = ",") {
   return(df_unnested)
 }
 
+
+#Autre test 
+unnest_columns_together <- function(df, colname1, colname2, sep = ",") {
+  # Nettoyer les colonnes en supprimant crochets et espaces
+  df[[colname1]] <- gsub("\\[|\\]", "", as.character(df[[colname1]]))
+  df[[colname1]] <- gsub("\\s+", "", df[[colname1]])
+  
+  df[[colname2]] <- gsub("\\[|\\]", "", as.character(df[[colname2]]))
+  df[[colname2]] <- gsub("\\s+", "", df[[colname2]])
+  
+  # Convertir les colonnes en liste de valeurs
+  df[[colname1]] <- strsplit(as.character(df[[colname1]]), sep)
+  df[[colname2]] <- strsplit(as.character(df[[colname2]]), sep)
+  
+  # Vérifier si les deux colonnes ont le même nombre d'éléments après split
+  df_unnested <- df %>%
+    rowwise() %>%
+    mutate(
+      # Créer une nouvelle ligne pour chaque année et valeur
+      year = list(df[[colname1]]), 
+      value = list(df[[colname2]]),
+      n = length(year[[1]])  # Compter le nombre d'éléments dans chaque ligne
+    ) %>%
+    unnest(cols = c(year, value)) %>%
+    select(-n)  # Retirer la colonne temporaire 'n'
+  
+  return(df_unnested)
+}
+
+
+# Applique la fonction sur ton dataframe sans modifier l'objet original
+donnees_comb_unnested <- unnest_columns_together(donnees_comb, "years", "values")
+
+
+View(donnees_comb_unnested)
+
+
+
+
+
+
+
+
