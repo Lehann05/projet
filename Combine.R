c695cc42508700424efaa931cd7dd5bfa49328d0
Lehann05 <lehannb2213@gmail.com>
2025-04-17 14:27:47 -0400

    Ajout d'une ligne pour tenter d'arranger le problème qui n'en était pas un

--- Combine.R
+++ Combine.R
@@ -17,8 +17,8 @@ combiner_csv <- function(dossier_principal, exclusion, nom_sortie, colonne, corr
   }
   
   # Lire et combiner
-  donnees_combinees <- lapply(liste_fichiers, read.csv) %>%
-    bind_rows()
+  donnees_combi <- lapply(liste_fichiers, read.csv) %>% mutate(years = as.character(years))
+  donnees_combinees <- bind_rows(donnees_combi)
   
   # Exporter le résultat
   write.csv(donnees_combinees, file = nom_sortie, row.names = FALSE)
