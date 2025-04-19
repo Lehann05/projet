ddd955b96e919d38fff9e6c357b97cbd5023cf24
Kelianne235 <stpk1601@Usherbrooke.ca>
2025-04-19 13:05:06 -0400

    Ajout de commentaires

--- R/open_corr.R
+++ R/open_corr.R
@@ -1,7 +1,7 @@
 #fonction pour ouvrir un dossier et corriger le nom d'une colonne
 #mettre argument entre "" pour que ça fonctionne
 ouverture <- function(direction, nom_colonne, nom_corriger){
-  data <- read.csv(direction)
-  colnames(data)[colnames(data) == nom_colonne] <- nom_corriger
-  return(data)
+  data <- read.csv(direction) # Ouvrir le fichier
+  colnames(data)[colnames(data) == nom_colonne] <- nom_corriger # Mettre le nouveau nom corrigé 
+  return(data) 
 }
