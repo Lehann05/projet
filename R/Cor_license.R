5b8bfe4a0532e8f51ffc6fcf015810c1ee815e3b
Kelianne235 <stpk1601@Usherbrooke.ca>
2025-04-19 12:49:48 -0400

    Ajout de commentaires

--- R/Cor_license.R
+++ R/Cor_license.R
@@ -1,18 +1,24 @@
+# Fonction pour corriger lisense en license 
 corriger_license <- function(data) {
 
+  #Vérifie si la colonne lisense existe 
   if("lisense" %in% names(data)) {
+    # Si la colonne license n'existe pas, renomme la colonne lisense en license
     if(!("license" %in% names(data))) {
       data <- data %>%
-        rename(license = lisense)
-      cat("Colonne 'lisense' renommée en 'license'.\n")
+        rename(license = lisense) # Renomme la colonne
+      cat("Colonne 'lisense' renommée en 'license'.\n") # Nous indique si la colonne a été renommée
+      
+      #si la colonne license existe, fusionne license et lisense ensemble
     } else {
       data <- data %>%
         mutate(license = ifelse(is.na(license) | license == "", lisense, license)) %>%
-        select(-lisense)
-      cat("Colonnes 'license' et 'lisense' fusionnées dans 'license'.\n")
+        select(-lisense) # Retire la colonne lisense 
+      cat("Colonnes 'license' et 'lisense' fusionnées dans 'license'.\n") # Nous indique si les colonnes ont été fusionnées
     }
+    
   } else {
-    cat("Pas de colonne 'lisense' détectée. Aucune modification.\n")
+    cat("Pas de colonne 'lisense' détectée. Aucune modification.\n") # Nous indique qu'il n'y a pas de colonne lisense
   }
   
   return(data)
