5325cf67dc9fecd28f2afa2a8dc77ef5078edd25
Kelianne235 <stpk1601@Usherbrooke.ca>
2025-04-19 13:03:52 -0400

    Arranger la mise en page

--- R/enl_col_vide.R
+++ R/enl_col_vide.R
@@ -5,10 +5,10 @@ enlever_colonnes_vides <- function(data) {
   # Sélectionne uniquement les colonnes qui ne sont pas entièrement NA
   data_nettoye <- data[, colSums(!is.na(data)) > 0]
   
-  #Permet de savoir combien de colonne ont été supprimées (supposé être 2) 
+  # Permet de savoir combien de colonne ont été supprimées (supposé être 2) 
   nb_supprimees <- ncol(data) - ncol(data_nettoye)
   
-  #Envoyer un message pour savoir si ça l'a fonctionné et combien de colonnes ont été enlevées
+  # Envoyer un message pour savoir si ça l'a fonctionné et combien de colonnes ont été enlevées
   cat(paste0(nb_supprimees, " Les colonnes ont été supprimées avec succès.\n"))
   
   return(data_nettoye)
