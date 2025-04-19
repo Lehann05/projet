9fe866cfe73b8f61f67bef787c41322cc47bcb67
Kelianne235 <stpk1601@Usherbrooke.ca>
2025-04-19 12:43:23 -0400

    Ajout de commentaires dans la fonction

--- R/Cor_geom.R
+++ R/Cor_geom.R
@@ -1,18 +1,24 @@
+# Fonction qui permet de combiner la colonne geom et geometry
 corriger_geom <- function(data) {
   
+  # Vérifie qu'il y a une colonne geometry 
   if("geometry" %in% names(data)) {
+    # Si la colonne 'geom' n'existe pas, renomme la colonne 'geometry' en 'geom'
     if(!("geom" %in% names(data))) {
       data <- data %>%
-        rename(geom = geometry)
-      cat("Colonne 'geometry' renommée en 'geom'.\n")
+        rename(geom = geometry) # Si la colonne existe, elle devient geom
+      cat("Colonne 'geometry' renommée en 'geom'.\n") # Nous indique si cela a fonctionné
+      
+      # Si la colonne 'geom' existe déjà, on fusionne les données de 'geometry' avec 'geom'
+      # On garde la valeur de 'geom' si présente, sinon on prend la valeur de 'geometry'
     } else {
       data <- data %>%
         mutate(geom = ifelse(is.na(geom) | geom == "", geometry, geom)) %>%
-        select(-geometry)
-      cat("Colonnes 'geom' et 'geometry' fusionnées dans 'geom'.\n")
+        select(-geometry) # Retire la colonne geometry 
+      cat("Colonnes 'geom' et 'geometry' fusionnées dans 'geom'.\n") # Nous indique qu'il y a eu une fusion
     }
   } else {
-    cat("Pas de colonne 'geometry' détectée. Aucune modification.\n")
+    cat("Pas de colonne 'geometry' détectée. Aucune modification.\n") # Nous indique si la colonne geometry n'existe pas
   }
   
   return(data)
